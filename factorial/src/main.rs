9110ef630650da055f1bee6ccc4a3045c1d6e1f5
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-04-06 12:07:59 +0100

    factorial complete

--- factorial/src/main.rs
+++ factorial/src/main.rs
@@ -1,11 +1,21 @@
-fn main() {
+use std::io;
 
-    let number = 5;
+fn main() {
+    println!("Enter a number to find the factorial of that number.");
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number = match number.trim().parse(){
+        Ok(num) => num,
+        Err(_) => 0,};
 
     println!("The factorial of {} is {}", number, factorial(number));
 }
 
-fn factorial(number : i32) -> i32{
+fn factorial(number: i32) -> i32 {
     let mut result = 1;
     for i in 1..=number {
         result *= i;
